88dd5621cdef0170992e89d14bfe56a52e705265
Atharv Gokule <gokuleatharv06@gmail.com>
2026-08-07 11:54:52 -0400

    feat(font): added list of font icons that will be used

---
+++ src/FontIcons.hpp
@@ -0,0 +1,23 @@
+// list of icons used by the program
+// icons can be searched for on https://www.nerdfonts.com/cheat-sheet
+
+#pragma once
+
+#define NF_FA_ARROW_RIGHT_LONG "\uf178"
+#define NF_FA_ARROW_LEFT_LONG "\uf177"
+#define NF_FA_PERSON_RUNNING "\uef0c"
+#define NF_FA_MOON "\uf186"
+
+// for some reason, imgui displays this as nf-fa-dice
+// or "\uedec"
+#define NF_OCT_SUN "\uf522"
+
+#define NF_WEATHER_DAY_SUNNY "\ue30d"
+
+#define NF_MD_WEIGHT_KILOGRAM "\U000f05a2"
+#define NF_MD_RADIUS_OUTLINE "\U000f0cc1"
+#define NF_MD_EARTH "\U000f01e7"
+#define NF_MD_MOON_NEW "\U000f0f64"
+
+#define NF_COD_QUESTION "\ueb32"
+
