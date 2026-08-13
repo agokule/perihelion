04c61ffb114fada44acae1f42c4f894f9a09ffbd
Atharv Gokule <gokuleatharv06@gmail.com>
2026-08-13 11:44:19 -0400

    feat(SimulationScreen): added get_objects() method to get vector of objects

--- src/SimulationScreen.hpp
+++ src/SimulationScreen.hpp
@@ -89,6 +89,8 @@ public:
 
     Object& get_object(size_t idx) { return scene.objects.at(idx); }
 
+    const std::vector<Object>& get_objects() const { return scene.objects; }
+
     int current_selected_object = -1;
 
 private:
